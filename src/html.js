5a75eaa080a9160b251f29c07247bc291e40d6d8
Martin Pravda <martin.pravda@gmail.com>
2024-03-29 12:23:35 +0100

    Offline mode

--- src/html.js
+++ src/html.js
@@ -1,21 +1,19 @@
-function create_html(script) {
+function create_html(script, tile_cache = {}) {
     return [
         "<!DOCTYPE html>",
         "<html>",
         "<head>",
-        "<title>Mapier.js</title>",
-        "</head>",
+        "<title>Mapier.js<\/title>",
+        "<\/head>",
         "<body>",
-        "<div><button id=\"save\">Save me</button></div>",
-        "<div id=\"map\" style=\"width: 100%; height: 400px\"></div>",
-        "<div id=\"store\"><script data-id=\"store\"><\/script></div>",
-        "<div id=\"core\">",
-        "<script data-id='core'>",
+        "<div><button id=\"save\">Save me<\/button><\/div>",
+        "<div id=\"map\" style=\"width: 100%; height: 400px\"><\/div>",
+        "<div id=\"store\"><script data-id=\"store\">'" + JSON.stringify(tile_cache) + "'<\/script><\/div>",
+        "<div id=\"core\"><script data-id=\"core\">",
         script,
-        "<\/script>",
-        "</div>",
-        "</body>",
-        "</html>"
+        "<\/script><\/div>",
+        "<\/body>",
+        "<\/html>"
     ].join("\n");
 }
 
--- src/mapier.js
+++ src/mapier.js
@@ -3,13 +3,22 @@
 */
 
 /*property
-    addEventListener, appendChild, body, click, createElement, createObjectURL,
-    download, freeze, getElementById, hidden, href, innerHTML, preventDefault,
-    querySelector, type
+    addEventListener, appendChild, body, center, click, createElement,
+    createObjectURL, crossOrigin, download, drawImage, fillRect, getContext,
+    getElementById, getImage, getTileCoord, height, hidden, href, innerHTML,
+    join, layers, onerror, onload, parse, preventDefault, querySelector,
+    removeChild, setTileLoadFunction, source, src, target, textContent,
+    toDataURL, type, url, view, width, zoom
 */
 
+import Map from "ol/Map";
+import View from "ol/View";
+import TileLayer from "ol/layer/Tile";
+import XYZ from "ol/source/XYZ";
 import create_html from "./html.js";
-import map from "./map.js";
+
+const tile_width = 256;
+const tile_height = 256;
 
 function save(filename, content, type) {
     const a = document.createElement("a");
@@ -24,15 +33,80 @@ function save(filename, content, type) {
 }
 
 function init() {
-    const button = document.getElementById("save");
-    const core_script = document.querySelector("script[data-id=\"core\"]").textContent;
+    const save_button = document.getElementById("save");
+    const core_script = document.querySelector(
+        "script[data-id=\"core\"]"
+    ).textContent;
+    const store_data = document.querySelector(
+        "script[data-id=\"store\"]"
+    ).textContent;
 
-    button.addEventListener("click", function (e) {
-        e.preventDefault();
-        save("mapier.html", create_html(core_script), "text/html");
+    let tile_cache;
+
+    try {
+        tile_cache = JSON.parse(store_data);
+    } catch (ignore) {
+        tile_cache = {};
+    }
+
+    const view = new View({
+        center: [0, 0],
+        zoom: 2
+    });
+    const osm_source = new XYZ({
+        url: "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+    });
+    const tile_layer = new TileLayer({source: osm_source});
+    const map = new Map({
+        layers: [tile_layer],
+        target: "map",
+        view
     });
 
-    map.tile_osm();
+    osm_source.setTileLoadFunction(function (tile, src) {
+        const cache_key = `OSM_${tile.getTileCoord().join("_")}`;
+
+        if (tile_cache[cache_key]) {
+            tile.getImage().src = tile_cache[cache_key];
+            return;
+        }
+
+        const image = new Image();
+        image.crossOrigin = "anonymous";
+        image.onload = function () {
+            const canvas = document.createElement("canvas");
+            const ctx = canvas.getContext("2d");
+
+            canvas.width = `${tile_width}`;
+            canvas.height = `${tile_height}`;
+            ctx.drawImage(image, 0, 0);
+
+            tile_cache[cache_key] = canvas.toDataURL("image/png");
+            tile.getImage().src = src;
+        };
+        image.src = src;
+        image.onerror = function (ignore) {
+            const canvas = document.createElement("canvas");
+            const ctx = canvas.getContext("2d");
+
+            canvas.width = `${tile_width}`;
+            canvas.height = `${tile_height}`;
+            ctx.fillRect(0, 0, tile_width, tile_height);
+            tile.getImage().src = canvas.toDataURL();
+        };
+    });
+
+
+    save_button.addEventListener("click", function (e) {
+        e.preventDefault();
+        save(
+            "mapier.html",
+            create_html(core_script, tile_cache),
+            "text/html"
+        );
+    });
+
+    return map;
 }
 
 function load() {
