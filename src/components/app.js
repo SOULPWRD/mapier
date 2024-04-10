13293bd9d2ac144c81a6d824aeb4c6ae47ba17f7
Martin Pravda <martin.pravda@gmail.com>
2024-04-10 19:24:39 +0200

    Base styler

--- src/components/app.js
+++ src/components/app.js
@@ -4,11 +4,26 @@
 
 import {h} from "vue";
 import make_map from "./map.js";
+import fstyle from "./fstyle.js";
+import use_fstyle from "./composables/use_fstyle.js"
+
+const base_styler = fstyle.css(function base() {
+    return `
+        body {
+            margin: 0;
+            padding: 0;
+        }
+    `;
+});
 
 function make_app() {
     function setup() {
+        const base_classes = use_fstyle(() => base_styler());
+
         return function render() {
-            return h("div", [
+            return h("div", {
+                class: base_classes.value
+            }, [
                 h(make_map())
             ]);
         };
