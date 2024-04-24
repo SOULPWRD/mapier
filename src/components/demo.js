811a09cf9bbc4b8ebbe94ab337d4de2be588e2ea
Martin Pravda <martin.pravda@gmail.com>
2024-04-24 10:42:55 +0200

    demo styles

--- src/components/demo.js
+++ src/components/demo.js
@@ -6,6 +6,8 @@ import {createApp} from "vue";
 
 function run(component, props = {}) {
     document.documentElement.innerHTML = "";
+    document.body.style.margin = 0;
+    document.body.style.padding = 0;
     const div = document.createElement("div");
     document.body.append(div);
     createApp(component, props).mount(div);
